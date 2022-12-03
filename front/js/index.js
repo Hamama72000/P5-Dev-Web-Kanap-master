8c049f53fef22d23b1c9857e629cbb8d44ace726
Hamama Assadi <hmima495gmail.com>
2022-12-03 14:07:41 +0100

    index.js terminé

--- front/js/index.js
+++ front/js/index.js
@@ -1,58 +1,59 @@
-const articles = document.getElementById('items');
-
+const items = document.getElementById("items");
 
 // Fonction pour récupérer les produits depuis l'API
-function getArticles() {
+function fetchArticles() {
   fetch("http://localhost:3000/api/products")
-    .then(res => res.json()) //recupération du résultat
-    .then(articles=> articles) //recuperation de la vraie valeur du résultat(permettra dexploiter l'api)
-    .catch(err => console.log(err)) 
+    .then(function (res) {
+      if (res.ok) {
+        return res.json();
+      }
+    })
+
+    .then(function (article) {
+      const articles = URL.createObjectURL(article);
+      console.log(article);
+    })
+
+.catch(function (error) {
+      alert(error);
+      // Une erreur est survenue
+    });
 }
-
-
 
 // Création  du modèle produit
 const createKanap = (kanap) => {
-  const image =  document.createElement('img');
-    image.setAttribute('src', `${kanap.imageUrl}`);
-    image.setAttribute('alt', `${kanap.altTxt}`);
+  const image = document.createElement("img");
+  image.setAttribute("src", `${kanap.imageUrl}`);
+  image.setAttribute("alt", `${kanap.altTxt}`);
 
-  const title = document.createElement('h3');
-    title.classList.add('productName');
-    title.textContent = `${kanap.name}`;
+  const title = document.createElement("h3");
+  title.classList.add("productName");
+  title.textContent = `${kanap.name}`;
 
-  const description = document.createElement('p');
-    description.classList.add('productDescription');
-    description.textContent = `${kanap.description}`;
+  const description = document.createElement("p");
+  description.classList.add("productDescription");
+  description.textContent = `${kanap.description}`;
 
-  const article = document.createElement('article');
-    article.appendChild(image);
-    article.appendChild(h3);
-    article.appendChild(p);
-  
-  const lien = document.createElement('a');
-    lien.setAttribute('href', `./product.html?id=${kanap._id}`);
-    lien.appendChild(article);
- 
+  const article = document.createElement("article");
+  article.appendChild(image);
+  article.appendChild(h3);
+  article.appendChild(p);
+
+  const lien = document.createElement("a");
+  lien.setAttribute("href", `./product.html?id=${kanap._id}`);
+  lien.appendChild(article);
+
   return lien;
+};
+
+// Rendu de la page d'accueil
+
+async function main() {
+
+  const getDatas = await fetchArticles();
+  for (article of getDatas) {
+    createKanap(article);
+    items.appendChild(createKanap(article));
+  }
 }
-
- // Rendu de la page d'accueil
-const article = getArticles(articles) => {
- 
-  // Destination des éléments
-  document.getElementsByClassName("item__img")[0].appendChild(img);
-  document.getElementById("title").innerText = product.name;
-  document.getElementById("price").innerText = product.price + " ";
-  document.getElementById("description").innerText = product.description;
-
-  // Boucle forEach pour ajouter toutes les couleurs en option du select en HTML
-  product.colors.forEach(function (color) {
-    const option = document.createElement("option");
-    const select = document.getElementById("colors");
-
-    // Récupération des données de l'API
-    option.value = color;
-    option.innerText = color;
-  });
-}
+main();
